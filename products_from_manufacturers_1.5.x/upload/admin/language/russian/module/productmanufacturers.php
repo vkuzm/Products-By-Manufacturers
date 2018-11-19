2884510e442b8bcf36cebf9ecf62bd37636ba2a8
Vitalii <this.azeroth@gmail.com>
2018-11-19 13:20:07 +0200

    Add files via upload

---
+++ products_from_manufacturers_1.5.x/upload/admin/language/russian/module/productmanufacturers.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']       = 'Товары от производителей';
+
+// Text
+$_['text_module']         = 'Модули';
+$_['text_success']        = 'Модуль успешно обновлен!';
+$_['text_content_top']    = 'Верх страницы';
+$_['text_content_bottom'] = 'Низ страницы';
+$_['text_column_left']    = 'Левая колонка';
+$_['text_column_right']   = 'Правая колонка';
+
+// Entry
+$_['entry_limit']         = 'Лимит:';
+$_['entry_image']         = 'Изображение (Ширина x Высота):';
+$_['entry_title']         = 'Заголовок:';
+$_['entry_brand']         = 'Бренд:';
+$_['entry_layout']        = 'Страница:';
+$_['entry_position']      = 'Расположение:';
+$_['entry_status']        = 'Статус:';
+$_['entry_sort_order']    = 'Порядок сортировки:';
+
+// Error
+$_['error_permission']    = 'У Вас нет прав для изменения модуля!';
+$_['error_image']         = 'Введите размеры изображения!';
+?>
---
+++ products_from_manufacturers_1.5.x/upload/catalog/language/english/module/productmanufacturers.php
@@ -0,0 +1,7 @@
+<?php
+// Heading 
+$_['heading_title'] = 'Product from manufacturers';
+
+// Text
+$_['text_reviews']  = 'Based on %s reviews.'; 
+?>
